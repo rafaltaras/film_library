70a63f6ba1c57d85ac088c452c12cb563886ea6f
taraskarafal <rafal.taraska@orange.com>
2022-11-27 09:20:52 +0100

    version 1.2

--- film_library.py
+++ film_library.py
@@ -1,6 +1,6 @@
 import random
 
-class Films():
+class Film():
     def __init__(self, title, year, type, repeated):
         self.title = title
         self.year = year
@@ -16,7 +16,7 @@ class Films():
     def play(self):
         self.repeated += 1 
 
-class Serials(Films):
+class Serial(Film):
     def __init__(self, episode, season, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.episode = episode
@@ -26,34 +26,32 @@ class Serials(Films):
         return f"{self.title} S{self.episode}E{self.season}"
 
 
-class Move_Datadase():
+class MoveDatadase():
     def __init__(self):
         self.movie_database = []
 
-    def database(self,film_serial): 
-        self.movie_database.append(film_serial)
+    def insert(self, entry): 
+      self.movie_database.append(entry)
 
     def get_movies(self):
         films_list = []
         for movie in self.movie_database:
-            if isinstance(movie, Serials) == False:
+            if isinstance(movie, Serial) == False:
                 films_list.append(movie)
         return films_list
         
     def get_serial(self):
         serials_list = []
         for serial in self.movie_database:
-            if isinstance(serial, Serials) == True:
+            if isinstance(serial, Serial) == True:
                 serials_list.append(serial)
         return serials_list   
 
 # Cos mi nie do końca działa ta funkcja
     def search(self, title):
-        self.title = title
-        if self.title == self.movie_database:
-            print (f'Pozycja {self.title} w kolekcji')
-        else:
-            print (f'Pozycji {self.title} brak w kolekcji')
+        for entry in collection.movie_database:
+            if entry.title == title:
+                return entry
 
     def generate_views(self, quantity):
         x = 0
@@ -64,23 +62,23 @@ class Move_Datadase():
     def top_titles(self, toptitle, quantity):
         self.toptitle = toptitle
         for i in range (0, quantity):
-            if isinstance(toptitle, Serials) == True:
+            if isinstance(toptitle, Serial) == True:
                print(f"Top film to: {toptitle}")
             else:
                 print(f"Top serial to: {toptitle}")
                         
       
 
-film = Films(title='Bond', year=2020, type='Dramat', repeated=7)
-film1 = Films(title='Rocky', year=2020, type='Dramat', repeated=7)
-serial = Serials(title='Gra o Tron', year=2011, type='Fantasy', repeated=7, episode=2, season=1)
-serial1 = Serials(title='Narcos', year=2011, type='Fantasy', repeated=7, episode=2, season=1)
+film = Film(title='Bond', year=2020, type='Dramat', repeated=7)
+film1 = Film(title='Rocky', year=2020, type='Dramat', repeated=7)
+serial = Serial(title='Gra o Tron', year=2011, type='Fantasy', repeated=7, episode=2, season=1)
+serial1 = Serial(title='Narcos', year=2011, type='Fantasy', repeated=7, episode=2, season=1)
 
-collection = Move_Datadase()
-x = collection.database(film)
-x = collection.database(film1)
-x = collection.database(serial)
-x = collection.database(serial1)
+collection = MoveDatadase()
+collection.insert(film)
+collection.insert(film1)
+collection.insert(serial)
+collection.insert(serial1)
 print('Baza filmów')
 for i in collection.movie_database:
     print(i)
@@ -101,11 +99,13 @@ for i in by_title:
 
 print('')
 print("SEARCH")
-collection.search('Rocky')
+entry = collection.search('Bond')
+print(entry)
 
 print('')
 print("RANDOM CHOICE")
-collection.generate_views(10)
+entry = collection.generate_views(10)
+print(entry)
 
 print('')
 print("TOP TITLES")
